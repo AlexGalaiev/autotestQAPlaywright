69e03a771c6e5ca1174734c692ae8673afe1e172
AlexGalaiev <sashagalaievsecond@gmail.com>
2024-01-06 13:20:44 +0200

    3commit

--- tests/test_KeyWey_fullregistration.spec.ts
+++ tests/test_KeyWey_fullregistration.spec.ts
@@ -1,12 +1,34 @@
 import { expect, test } from "@playwright/test";
+import { ShortRegistrationPage } from "../pageObjects/createAccount";
+import { SuccessAccountCreationPopup } from "../pageObjects/components/popups/successAccountCreationPopup";
+import { ResidenceAndCitizenship } from "../pageObjects/fullRegistration/Registartion_MainPage/residenceAndCitizenship";
+import { ChangeCompanyPopup } from "../pageObjects/components/popups/changeCompanyPopup";
+import { PersonalDetails } from "../pageObjects/fullRegistration/Registartion_MainPage/personalDetails";
+import { ResidenceAdress } from "../pageObjects/fullRegistration/Registartion_MainPage/residenceAdress";
+import { TinPage } from "../pageObjects/fullRegistration/Registartion_MainPage/tin";
 
 
 
 test.beforeEach( async ({page}) =>{
-    
+    let shortRegistration = new ShortRegistrationPage(page);
+        await shortRegistration.goto();
+        await shortRegistration.createCFDUser();
+        let successPopup = new SuccessAccountCreationPopup(page);
+        await successPopup.continueRegistration();
+        let residencePage = new ResidenceAndCitizenship(page);
+        await residencePage.changeCountry("Poland");
+        await residencePage.fillResidenceAndCitizenshipSte();
+        let changeCompanyPopup = new ChangeCompanyPopup(page);
+        await changeCompanyPopup.proceedChangeCompanyPopup();
+        let personalDetails = new PersonalDetails(page);
+        await personalDetails.fillPersonalDetails();
+        let residenceAdrees = new ResidenceAdress(page);
+        await residenceAdrees.fillResidenceAdress();
+        let tinPage = new TinPage(page);
+        await tinPage.fillTinPage();
 })
 
 test('Key Wey HIGH SCORE Full REgistration', async({ page }) => {
-    
+    console.log("edrer")
 
 })
