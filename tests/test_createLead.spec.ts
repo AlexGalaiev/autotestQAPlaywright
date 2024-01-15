c8f606cd0c714ffcbf5faa0246382b7514f07897
Alex Galaiev <sashagalaievsecond@gmail.com>
2024-01-15 19:39:01 +0200

    5

--- tests/test_createLead.spec.ts
+++ tests/test_createLead.spec.ts
@@ -1,16 +1,16 @@
 import { expect, test } from "@playwright/test";
 import { SuccessAccountCreationPopup } from "../pageObjects/components/popups/successAccountCreationPopup.ts";
 import { ShortRegistrationPage } from "../pageObjects/createAccount.ts";
-import { ProjectLocalizationEN } from "../pageObjects/localization/localization_en.ts";
+// import { ProjectLocalizationEN } from "../pageObjects/localization/localization_en.ts";
 
 
 
-test('Create CFD user via short registration form', async({ page }) => {
-    let shortRegistrationForm = new ShortRegistrationPage(page);
-    let projectLocalization = new ProjectLocalizationEN();
-    await shortRegistrationForm.goto();
-    await shortRegistrationForm.createCFDUser();
-    let successPopup = new SuccessAccountCreationPopup(page);
-    await expect(successPopup.getSuccessPopupHeader).toBeVisible;
-    await expect(successPopup.getSuccessPopupHeader).toHaveText(projectLocalization.getValue("successPopupHeader"));
-})
+// test('Create CFD user via short registration form', async({ page }) => {
+//     let shortRegistrationForm = new ShortRegistrationPage(page);
+//     let projectLocalization = new ProjectLocalizationEN();
+//     await shortRegistrationForm.goto();
+//     await shortRegistrationForm.createCFDUser();
+//     let successPopup = new SuccessAccountCreationPopup(page);
+//     await expect(successPopup.getSuccessPopupHeader).toBeVisible;
+//     await expect(successPopup.getSuccessPopupHeader).toHaveText(projectLocalization.getValue("successPopupHeader"));
+// })
